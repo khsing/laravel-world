2b4c5382cac2a68e324aa8d74fd19fbce2b03afe
Mathieu Leblond <mathieu.leblond@international.gc.ca>
2021-10-12 09:19:36 -0400

    - removed duuplicates in Divisions - updated city lists with cities with 5000+ population from Geonames Dump.
    Source: https://download.geonames.org/export/dump/

--- database/seeders/WorldContinentsTableSeeder.php
+++ database/seeders/WorldContinentsTableSeeder.php
@@ -14,55 +14,55 @@ class WorldContinentsTableSeeder extends Seeder
      */
     public function run()
     {
+        
 
-
-        \DB::table('world_continents')->truncate();
-
+        \DB::table('world_continents')->delete();
+        
         \DB::table('world_continents')->insert(array (
-            0 =>
+            0 => 
             array (
                 'id' => 1,
                 'name' => 'Asia',
                 'code' => 'as',
             ),
-            1 =>
+            1 => 
             array (
                 'id' => 2,
                 'name' => 'Europe',
                 'code' => 'eu',
             ),
-            2 =>
+            2 => 
             array (
                 'id' => 3,
                 'name' => 'Africa',
                 'code' => 'af',
             ),
-            3 =>
+            3 => 
             array (
                 'id' => 4,
                 'name' => 'Oceania',
                 'code' => 'oc',
             ),
-            4 =>
+            4 => 
             array (
                 'id' => 5,
                 'name' => 'Antarctica',
                 'code' => 'an',
             ),
-            5 =>
+            5 => 
             array (
                 'id' => 6,
                 'name' => 'North America',
                 'code' => 'na',
             ),
-            6 =>
+            6 => 
             array (
                 'id' => 7,
                 'name' => 'South America',
                 'code' => 'sa',
             ),
         ));
-
-
+        
+        
     }
 }
